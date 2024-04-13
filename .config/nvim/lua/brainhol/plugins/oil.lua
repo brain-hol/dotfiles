d8c9e5b65279596869365a8e0e15bc22067d0223
Brian Holderness <brian@holderness.net>
2024-04-12 18:05:18 -0600

    Change oil config

--- .config/nvim/lua/brainhol/plugins/oil.lua
+++ .config/nvim/lua/brainhol/plugins/oil.lua
@@ -1,9 +1,14 @@
 return {
 	"stevearc/oil.nvim",
-	config = function()
-		vim.keymap.set("n", "-", "<cmd>Oil --float<cr>", { desc = "Open parent directory" })
-		require("oil").setup()
-	end,
-	opts = {},
+	-- False so that Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+	lazy = false,
+	keys = {
+		{ "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+	},
+	opts = {
+		view_options = {
+			show_hidden = true,
+		},
+	},
 }
 
