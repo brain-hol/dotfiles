4b855e26a44f9d82e10b5bf3cf40ebd5fc78fb78
Brian Holderness <brian@holderness.net>
2024-12-18 23:16:04 -0700

    feat: add neovim

---
+++ nvim/.config/nvim/lua/plugins/autocomplete.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'saghen/blink.cmp',
+    dependencies = 'rafamadriz/friendly-snippets',
+
+    version = 'v0.*',
+    opts = {
+      keymap = { preset = 'default' },
+
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = 'mono'
+      },
+
+      completion = {
+        documentation = {
+          auto_show = true,
+        },
+      },
+
+      signature = { enabled = true }
+    },
+  },
+}
